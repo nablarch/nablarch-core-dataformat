73fa4602299fb7d74588f8a82873306625884041
siosio <siosio12@gmail.com>
2016-11-04 11:22:37 +0900

    SignedNumberStringのテストを見直し
    * テストケース追加
    * 機能結合テストはクラスを分離
    
    #19

---
+++ src/test/java/nablarch/core/dataformat/convertor/value/SignedNumberStringIntegrationTest.java
@@ -0,0 +1,350 @@
+package nablarch.core.dataformat.convertor.value;
+
+import nablarch.core.dataformat.DataRecord;
+import nablarch.core.dataformat.DataRecordFormatter;
+import nablarch.core.dataformat.FormatterFactory;
+import nablarch.core.dataformat.InvalidDataFormatException;
+import nablarch.test.support.tool.Hereis;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import static nablarch.core.dataformat.DataFormatTestUtils.createInputStreamFrom;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * {@link SignedNumberString}の機能結合テスト。
+ * 
+ * @author Masato Inoue
+ */
+public class SignedNumberStringIntegrationTest {
+
+    private DataRecordFormatter formatter = null;
+    
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+    
+    /** フォーマッタを生成する。 */
+    private DataRecordFormatter createFormatter(File file) {
+        return FormatterFactory.getInstance().setCacheLayoutFileDefinition(false).createFormatter(file);
+    }
+    
+    private void createFile(File formatFile, String... lines) throws Exception {
+        BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream(formatFile), "utf-8"));
+        try {
+            for (final String line : lines) {
+                writer.write(line);
+                writer.write("\n");
+            }
+            writer.flush();
+        } finally {
+            writer.close();
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        final File formatFile = temporaryFolder.newFile("format.dat");
+        createFile(formatFile,
+                "file-type: \"Variable\"",
+                "text-encoding: \"ms932\"",
+                "record-separator: \"\\n\"",
+                "field-separator: \",\"",
+                "",
+                "[TestDataRecord]",
+                "1  amount1 X  signed_number",
+                "2  amount2 X  signed_number",
+                "3  amount3 X  signed_number"
+        );
+
+        formatter = createFormatter(formatFile);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (formatter != null) {
+            formatter.close();
+        }
+    }
+
+    /**
+     * 入力された文字列をBigDecimal型としてDataRecordに格納できることの確認。
+     */
+    @Test
+    public void read() throws Exception {
+        InputStream inputStream = createInputStreamFrom("12.345,-234.56,-345.67");
+        formatter.setInputStream(inputStream)
+                 .initialize();
+        
+        DataRecord record = formatter.readRecord();
+
+        assertThat((BigDecimal) record.get("amount1"), is(new BigDecimal("12.345")));
+        assertThat((BigDecimal) record.get("amount2"), is(new BigDecimal("-234.56")));
+        assertThat((BigDecimal) record.get("amount3"), is(new BigDecimal("-345.67")));
+    }
+    
+    /**
+     * NumberStringConvertorを使用し、
+     * DataRecordに設定された数値、Integer型の数値、BigDecimal型の数値が正常にファイルに出力されることの確認。
+     * (DBから取得したデータを書き出す場合の想定）
+     */
+    @Test
+    public void write() throws Exception {
+
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        formatter.setOutputStream(outputStream)
+                 .initialize();
+        
+        formatter.writeRecord(new DataRecord(){{
+            put("amount1", "+12.345"); // String Number
+            put("amount2", -234.56); // Integer
+            put("amount3", new BigDecimal(-34567)); // BigDecimal
+        }});
+
+        assertThat(outputStream.toString("ms932"), is("+12.345,-234.56,-34567\n"));
+    }
+    
+    /**
+     * 入力時に形式が符号付き数値でない場合に例外がスローされるテスト。
+     */
+    @Test
+    public void testFormatCheckOnRead() throws Exception {
+
+        SignedNumberString convertor = new SignedNumberString();
+        
+        String param = "a";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+
+        param = "1";
+        BigDecimal paramDecimal = new BigDecimal(1);
+        assertEquals(paramDecimal, convertor.convertOnRead(param));
+
+        param = "+1";
+        paramDecimal = new BigDecimal(1);
+        assertEquals(paramDecimal, convertor.convertOnRead(param));
+        
+        param = "-1";
+        paramDecimal = new BigDecimal(-1);
+        assertEquals(paramDecimal, convertor.convertOnRead(param));
+        
+        param = "+1.23";
+        paramDecimal = new BigDecimal(+1.23);
+        assertEquals(paramDecimal.intValue(), convertor.convertOnRead(param).intValue());
+
+        param = "/1.23";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+        param = ".23";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+        
+        param = "+.23";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+        
+        param = "-.23";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+        
+
+        param = "1.2.3";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+
+        param = ".9";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+        
+        param = "9.";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+        
+
+        param = "1..3";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+    }
+
+    /**
+     * 出力時に形式が符号付き数値でない場合に例外がスローされるテスト。
+     */
+    @Test
+    public void testFormatCheckOnWrite() throws Exception {
+
+        SignedNumberString convertor = new SignedNumberString();
+        
+        // 出力時の空文字は許容されない
+        String param = "";
+        try {
+            convertor.convertOnWrite(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+        
+        param = "a";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+
+        param = "1";
+        BigDecimal paramDecimal = new BigDecimal(1);
+        assertEquals(paramDecimal, convertor.convertOnRead(param));
+
+        param = "+1";
+        paramDecimal = new BigDecimal(1);
+        assertEquals(paramDecimal, convertor.convertOnRead(param));
+        
+        param = "-1";
+        paramDecimal = new BigDecimal(-1);
+        assertEquals(paramDecimal, convertor.convertOnRead(param));
+        
+        param = "+1.23";
+        paramDecimal = new BigDecimal(+1.23);
+        assertEquals(paramDecimal.intValue(), convertor.convertOnRead(param).intValue());
+
+        param = "/1.23";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+        param = ".23";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+        
+        param = "+.23";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+        
+        param = "-.23";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+        
+
+        param = "1.2.3";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+
+        param = ".9";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+        
+        param = "9.";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+        
+
+        param = "1..3";
+        try {
+            convertor.convertOnRead(param);
+            fail();
+        } catch (InvalidDataFormatException e) {
+            assertTrue(true);
+        }
+    }
+    
+    /**
+     * 出力時に符号が付いている場合にエラーが発生しないことの確認。
+     */
+    @Test
+    public void testWriteSign(){
+
+        SignedNumberString convertor = new SignedNumberString();
+
+        Number numberParam = new BigDecimal(-25.123455).setScale(5, RoundingMode.UP);
+        String convertedString = convertor.convertOnWrite(numberParam);
+        assertEquals("-25.12346", convertedString);
+        
+    }
+    
+    
+}
--- src/test/java/nablarch/core/dataformat/convertor/value/SignedNumberStringTest.java
+++ src/test/java/nablarch/core/dataformat/convertor/value/SignedNumberStringTest.java
@@ -1,398 +1,85 @@
 package nablarch.core.dataformat.convertor.value;
 
-import nablarch.core.dataformat.DataRecord;
-import nablarch.core.dataformat.DataRecordFormatter;
-import nablarch.core.dataformat.FormatterFactory;
-import nablarch.core.dataformat.InvalidDataFormatException;
-import nablarch.test.support.tool.Hereis;
-import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.text.IsEmptyString.isEmptyString;
+import static org.junit.Assert.*;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
-import static nablarch.core.dataformat.DataFormatTestUtils.createInputStreamFrom;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import org.hamcrest.CoreMatchers;
+
+import nablarch.core.dataformat.InvalidDataFormatException;
+import nablarch.core.dataformat.SyntaxErrorException;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
- * 符号付き数値コンバータのテストケース。
- * 
- * 観点：
- * 入力時、出力時に符号付き数値データの型変換が正常に行われることの確認および、
- * 形式チェックの細かい動作確認（異常系）を行う。
- * 
- * @author Masato Inoue
+ * {@link SignedNumberString}のテスト。
  */
 public class SignedNumberStringTest {
 
-    private DataRecordFormatter formatter = null;
+    private final SignedNumberString sut = new SignedNumberString();
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
     
-    /** フォーマッタを生成する。 */
-    private DataRecordFormatter createFormatter(File file) {
-        return FormatterFactory.getInstance().setCacheLayoutFileDefinition(false).createFormatter(file);
-    }
-
-    /**
-     * 入力された文字列をBigDecimal型としてDataRecordに格納できることの確認。
-     */
     @Test
-    public void testReadNumberStringConvertor() throws Exception {
-        File formatFile = Hereis.file("./format.dat");
-        /***********************************************************
-        file-type:         "Variable"         
-        text-encoding:     "ms932" # 文字列型フィールドの文字エンコーディング
-        record-separator:  "\n"       # レコード区切り文字
-        field-separator:   ","       # フィールド区切り文字
-        quoting-delimiter: "\""       # クオート文字
-
-        [TestDataRecord]
-        1  amount1        X  signed_number    # 数値1          
-        2  amount2        X  signed_number    # 数値2           
-        3  amount3        X  signed_number    # 数値3           
-        ************************************************************/
-        formatFile.deleteOnExit();
-        
-        formatter = createFormatter(formatFile);
-        
-        InputStream inputStream = createInputStreamFrom("\"12.345\",\"-234.56\",\"-345.67\"");
-        formatter.setInputStream(inputStream).initialize();
-        
-        DataRecord readRecord = formatter.readRecord();
-        assertEquals(new BigDecimal("+12.345"), readRecord.get("amount1"));
-        assertEquals(new BigDecimal("-234.56"), readRecord.get("amount2"));
-        assertEquals(new BigDecimal("-345.67"), readRecord.get("amount3"));
-        formatter.close();
-
-        new File("record.dat").deleteOnExit();
+    public void writeTest() throws Exception {
+        assertThat(sut.convertOnWrite("100"), is("100"));
+        assertThat(sut.convertOnWrite("-1.123"), is("-1.123"));
+        assertThat(sut.convertOnWrite("+500"), is("+500"));
+        assertThat(sut.convertOnWrite(BigDecimal.ONE), is("1"));
+        assertThat(sut.convertOnWrite(-1.123), is("-1.123"));
+        assertThat(sut.convertOnWrite(0x00), is("0"));
+        assertThat(sut.convertOnWrite(100L), is("100"));
     }
-    
-    /**
-     * NumberStringConvertorを使用し、
-     * DataRecordに設定された数値、Integer型の数値、BigDecimal型の数値が正常にファイルに出力されることの確認。
-     * (DBから取得したデータを書き出す場合の想定）
-     */
+
     @Test
-    public void testWriteNumberStringConvertor() throws Exception {
-        File formatFile = Hereis.file("./format.dat");
-        /***********************************************************
-        file-type:         "Variable"         
-        text-encoding:     "ms932" # 文字列型フィールドの文字エンコーディング
-        record-separator:  "\n"       # レコード区切り文字
-        field-separator:   ","       # フィールド区切り文字
-        quoting-delimiter: "\""       # クオート文字
-
-        [TestDataRecord]
-        1  amount1        X  signed_number    # 数値1          
-        2  amount2        X  signed_number    # 数値2           
-        3  amount3        X  signed_number    # 数値3           
-        ************************************************************/
-        formatFile.deleteOnExit();
-        
-        formatter = createFormatter(formatFile);
-        
-        FileOutputStream outputStream = new FileOutputStream("record.dat");
-        formatter.setOutputStream(outputStream).initialize();
-        
-        formatter.writeRecord(new DataRecord(){{
-            put("amount1", "+12.345"); // String Number
-            put("amount2", -234.56); // Integer
-            put("amount3", new BigDecimal(-34567)); // BigDecimal
-        }});
-        
-        formatter.close();
-        
-        BufferedReader reader = new BufferedReader(new InputStreamReader(
-                new FileInputStream("record.dat"), "ms932"
-           ));
-        
-        assertEquals("\"+12.345\",\"-234.56\",\"-34567\"", reader.readLine());
-        
-
-        outputStream.close();
-        new File("record.dat").deleteOnExit();
+    public void writeNull() throws Exception {
+        assertThat(sut.convertOnWrite(null), isEmptyString());
     }
 
-    /**
-     * 出力時に形式が符号付き数値でない場合に例外がスローされるテスト。
-     */
     @Test
-    public void testInvalidWriterNumberFormat() throws Exception {
-        File formatFile = Hereis.file("./format.dat");
-        /***********************************************************
-        file-type:         "Variable"         
-        text-encoding:     "ms932" # 文字列型フィールドの文字エンコーディング
-        record-separator:  "\n"       # レコード区切り文字
-        field-separator:   ","       # フィールド区切り文字
-        quoting-delimiter: "\""       # クオート文字
-
-        [TestDataRecord]
-        1  amount1        X  signed_number    # 数値1          
-        2  amount2        X  signed_number    # 数値2           
-        3  amount3        X  signed_number    # 数値3           
-        ************************************************************/
-        formatFile.deleteOnExit();
-        
-        formatter = createFormatter(formatFile);
-        
-        FileOutputStream outputStream = new FileOutputStream("record.dat");
-        formatter.setOutputStream(outputStream).initialize();
-        
-        try {
-            formatter.writeRecord(new DataRecord() {
-                {
-                    put("amount1", ".12345"); // String Number
-                    put("amount2", 234.56); // Integer
-                    put("amount3", new BigDecimal(34567)); // BigDecimal
-                }
-            });
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertEquals(
-                    "invalid parameter format was specified. parameter format must be [^[+-]?([0-9][0-9]*)?[0-9](\\.[0-9]*[0-9])?$]. value=[.12345]. field name=[amount1].",
-                    e.getMessage());
-        }
-        
-        formatter.close();
-        
+    public void writeNotNumeric_shouldThrowException() throws Exception {
+        expectedException.expect(InvalidDataFormatException.class);
+        expectedException.expectMessage("invalid parameter format was specified." 
+                + " parameter format must be [^[+-]?([0-9][0-9]*)?[0-9](\\.[0-9]*[0-9])?$]. value=[abc].");
+        sut.convertOnWrite("abc");
     }
 
-    /**
-     * 入力時の引数がnullおよび空文字の場合にnullが、出力時の引数がnullの場合に空文字が返却されることのテスト。
-     */
     @Test
-    public void testArgNullOrEmpty() throws Exception {
-        SignedNumberString numberString = new SignedNumberString();
-        assertNull(numberString.convertOnRead(null));
-        assertNull(numberString.convertOnRead(""));
-        assertEquals("", numberString.convertOnWrite(null));
+    public void writeEmptyString_shouldThrowException() throws Exception {
+        expectedException.expect(InvalidDataFormatException.class);
+        expectedException.expectMessage("invalid parameter format was specified." 
+                + " parameter format must be [^[+-]?([0-9][0-9]*)?[0-9](\\.[0-9]*[0-9])?$]. value=[].");
+        sut.convertOnWrite("");
     }
-    
-    /**
-     * 入力時に形式が符号付き数値でない場合に例外がスローされるテスト。
-     */
+
     @Test
-    public void testFormatCheckOnRead() throws Exception {
-
-        SignedNumberString convertor = new SignedNumberString();
-        
-        String param = "a";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-
-        param = "1";
-        BigDecimal paramDecimal = new BigDecimal(1);
-        assertEquals(paramDecimal, convertor.convertOnRead(param));
-
-        param = "+1";
-        paramDecimal = new BigDecimal(1);
-        assertEquals(paramDecimal, convertor.convertOnRead(param));
-        
-        param = "-1";
-        paramDecimal = new BigDecimal(-1);
-        assertEquals(paramDecimal, convertor.convertOnRead(param));
-        
-        param = "+1.23";
-        paramDecimal = new BigDecimal(+1.23);
-        assertEquals(paramDecimal.intValue(), convertor.convertOnRead(param).intValue());
-
-        param = "/1.23";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-        param = ".23";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-        
-        param = "+.23";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-        
-        param = "-.23";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-        
-
-        param = "1.2.3";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-
-        param = ".9";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-        
-        param = "9.";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-        
-
-        param = "1..3";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
+    public void readTest() throws Exception {
+        assertThat(sut.convertOnRead("100"), is(new BigDecimal("100")));
+        assertThat(sut.convertOnRead("-1.123"), is(new BigDecimal("-1.123")));
+        assertThat(sut.convertOnRead("+500"), is(new BigDecimal("500")));
     }
 
-    /**
-     * 出力時に形式が符号付き数値でない場合に例外がスローされるテスト。
-     */
     @Test
-    public void testFormatCheckOnWrite() throws Exception {
-
-        SignedNumberString convertor = new SignedNumberString();
-        
-        // 出力時の空文字は許容されない
-        String param = "";
-        try {
-            convertor.convertOnWrite(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-        
-        param = "a";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-
-        param = "1";
-        BigDecimal paramDecimal = new BigDecimal(1);
-        assertEquals(paramDecimal, convertor.convertOnRead(param));
-
-        param = "+1";
-        paramDecimal = new BigDecimal(1);
-        assertEquals(paramDecimal, convertor.convertOnRead(param));
-        
-        param = "-1";
-        paramDecimal = new BigDecimal(-1);
-        assertEquals(paramDecimal, convertor.convertOnRead(param));
-        
-        param = "+1.23";
-        paramDecimal = new BigDecimal(+1.23);
-        assertEquals(paramDecimal.intValue(), convertor.convertOnRead(param).intValue());
-
-        param = "/1.23";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-        param = ".23";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-        
-        param = "+.23";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-        
-        param = "-.23";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-        
-
-        param = "1.2.3";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-
-        param = ".9";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-        
-        param = "9.";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
-        
-
-        param = "1..3";
-        try {
-            convertor.convertOnRead(param);
-            fail();
-        } catch (InvalidDataFormatException e) {
-            assertTrue(true);
-        }
+    public void readNull() throws Exception {
+        assertThat(sut.convertOnRead(null), is(nullValue()));
     }
-    
-    /**
-     * 出力時に符号が付いている場合にエラーが発生しないことの確認。
-     */
+
     @Test
-    public void testWriteSign(){
-
-        SignedNumberString convertor = new SignedNumberString();
-
-        Number numberParam = new BigDecimal(-25.123455).setScale(5, RoundingMode.UP);
-        String convertedString = convertor.convertOnWrite(numberParam);
-        assertEquals("-25.12346", convertedString);
-        
+    public void readEmptyString() throws Exception {
+        assertThat(sut.convertOnRead(""), is(nullValue()));
     }
-    
-    
+
+    @Test
+    public void readNotNumeric_shouldThrowException() throws Exception {
+        expectedException.expect(InvalidDataFormatException.class);
+        expectedException.expectMessage("invalid parameter format was specified." 
+                + " parameter format must be [^[+-]?([0-9][0-9]*)?[0-9](\\.[0-9]*[0-9])?$]. value=[abc].");
+        sut.convertOnRead("abc");
+    }
 }
