2b4ee9f822303238c9bb88e4261871c9296ba568
kumano_goro <kumano.goro@tis.co.jp>
2021-01-19 11:43:56 +0900

    containsからequalsに変更したことで不要になったNullチェック削除

--- src/main/java/nablarch/core/util/JsonParser.java
+++ src/main/java/nablarch/core/util/JsonParser.java
@@ -362,14 +362,12 @@ public final class JsonParser {
      * 項目セパレータ検出時の処理です。
      */
     private void onItemSeparator() {
-        if (lastToken != null
-                && ("]".equals(lastToken) || "}".equals(lastToken))) {
+        if (("]".equals(lastToken) || "}".equals(lastToken))) {
             // オブジェクト、配列の終了処理内で必要な処理は完了しているので何もしない。
             return;
         }
-        if (lastToken != null
-                && ("[".equals(lastToken) || "{".equals(lastToken)
-                    || ",".equals(lastToken) || ":".equals(lastToken))) {
+        if (("[".equals(lastToken) || "{".equals(lastToken)
+                || ",".equals(lastToken) || ":".equals(lastToken))) {
             throw new IllegalArgumentException("value is requires");
         }
         if (currentList != null && currentKey == null) {
